5e710758db9f2d570c88e1576a880a5e1e2fee4b
Rebecca Mines <becca.mines22297@gmail.com>
2023-07-19 18:24:43 -0500

    Cleaned up base code a bit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,32 @@
 package org.example;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
 
     public static void main(String[] args) {
+        final int monthsInYear = 12;
+        final int percent = 100;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Please enter the principle: ");
         int principle = scanner.nextInt();
+
         System.out.print("Please enter the annual interest rate: ");
         float annualInterest = scanner.nextFloat();
+
         System.out.print("Please enter the length of the loan (in years): ");
         int period = scanner.nextInt();
 
-        float monthlyInterest = ((annualInterest / 100) / 12);
-        int numTotalPayments = (period * 12);
-        double mortgagePayment = (principle * (monthlyInterest * ((Math.pow(1 + monthlyInterest, numTotalPayments) / ((Math.pow(1 + monthlyInterest, numTotalPayments) - 1))))));
+        int numOfPayments = (period * 12);
+        float monthlyInterest = ((annualInterest / percent) / monthsInYear);
+        double mortgagePayment = (principle * (monthlyInterest * ((Math.pow(1 + monthlyInterest, numOfPayments) / ((Math.pow(1 + monthlyInterest, numOfPayments) - 1))))));
 
-        System.out.println((mortgagePayment));
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgagePayment);
+        System.out.println("Your monthly mortgage payment is: " + mortgageFormatted);
     }
 
 }
