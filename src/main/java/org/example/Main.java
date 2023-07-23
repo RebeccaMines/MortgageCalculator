d3eae971425a5d4a0c593ba6d934bd8d1e362d94
Rebecca Mines <becca.mines22297@gmail.com>
2023-07-23 00:11:54 -0500

    conditionals added, and grammar errors fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,28 +2,47 @@ package org.example;
 
 import java.text.NumberFormat;
 import java.util.Scanner;
-import java.text.DecimalFormat;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        final int monthsInYear = 12;
-        final int percent = 100;
+        final byte monthsInYear = 12;
+        final byte percent = 100;
+
+        int principal = 0;
+        float annualInterest = 0;
+        int period = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please enter the principle: ");
-        int principle = scanner.nextInt();
+        while (true) {
+            System.out.print("Please enter the principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Please enter a value between 1K and 1M.");
+        }
 
-        System.out.print("Please enter the annual interest rate: ");
-        float annualInterest = scanner.nextFloat();
+        while (true) {
+            System.out.print("Please enter the annual interest rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30 )
+                break;
+            System.out.println("Please enter a value between 0.00 and 30.00.");
+        }
 
-        System.out.print("Please enter the length of the loan (in years): ");
-        int period = scanner.nextInt();
+        while (true) {
+            System.out.print("Please enter the length of the loan (in years): ");
+            period = scanner.nextInt();
+            if (period >= 1 && period <= 30)
+                break;
+            System.out.println("Please enter a value between 0 and 30.");
+        }
 
         int numOfPayments = (period * 12);
         float monthlyInterest = ((annualInterest / percent) / monthsInYear);
-        double mortgagePayment = (principle * (monthlyInterest * ((Math.pow(1 + monthlyInterest, numOfPayments) / ((Math.pow(1 + monthlyInterest, numOfPayments) - 1))))));
+        double mortgagePayment = (principal * (monthlyInterest * ((Math.pow(1 + monthlyInterest, numOfPayments) / ((Math.pow(1 + monthlyInterest, numOfPayments) - 1))))));
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgagePayment);
         System.out.println("Your monthly mortgage payment is: " + mortgageFormatted);
